1bca2b393da7f54cb5168ebf8e549fc9a2e49c98
“aLoHaha1” <942844585@qq.com>
2021-09-13 19:19:53 +0800

    first commit

---
+++ 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    char x, y; //ASCII
+
+    //printf("x=");
+    //scanf("%c%c", &x, &y);
+    //y = x * x * x - 4 * x + 6;
+    //printf("%c,%c\n", x, y);
+
+    printf("%d\n", 'a');
+
+    return 0;
+}
